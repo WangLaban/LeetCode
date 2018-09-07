512d2454906e28c480ecdf966c805f016e33040a
WangLaban <42727870+WangLaban@users.noreply.github.com>
2018-09-07 17:30:17 +0800

    Update 30_SubstringwithConcatenationofAllWords.h
    Add solution

--- 30_SubstringwithConcatenationofAllWords.h
+++ 30_SubstringwithConcatenationofAllWords.h
@@ -14,7 +14,8 @@ Input:
 Output: []
 */
 // Use map to replace unordered_map is also OK.
-class Solution {
+// Solution1: 
+/*class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> ret;
@@ -44,5 +45,62 @@ public:
                 ret.push_back(i);
         }
         return ret;
+    }
+};*/
+
+// Solution2: refer to https://leetcode.com/submissions/detail/174391273/
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        if (s.empty() || words.empty())
+            return res;
+        unordered_map<string, int> expected;
+        for (auto &word : words)
+        {
+            expected[word]++;
+        }
+        
+        int word_len = words[0].size();
+        // take the input s = "barfoothefoobarman" and words = ["foo","bar"] for an example
+        // the substrings must start at i, i + 3, i + 6, ...
+        // so only 3 round is needed 
+        for (int i = 0; i < word_len; i++)
+        {
+            unordered_map<string, int> window;
+            // use this counter and the condition check of "window[word] > expected[word]" 
+            // we don't need to check the maps "window" and "expected" fully to see if they equal with each other
+            int cnt = 0;
+            int j = i;
+            int beg = j;
+            while (j <= (int)s.size() - word_len)
+            {
+                string word = s.substr(j, word_len);
+                if (expected.find(word) != expected.end())
+                {
+                    window[word]++;
+                    j += word_len;
+                    cnt++;
+                    while (window[word] > expected[word])
+                    {
+                        string left = s.substr(beg, word_len);
+                        window[left]--;
+                        cnt--;
+                        beg += word_len;
+                    }
+                    if (cnt == words.size())
+                        res.push_back(beg);
+                }
+                else
+                {
+                    // when some thing not expected are met, we have to drop everything we've got so far 
+                    window.clear();
+                    cnt = 0;
+                    j += word_len;
+                    beg = j;
+                }
+            }
+        }
+        return res;
     }
 };
