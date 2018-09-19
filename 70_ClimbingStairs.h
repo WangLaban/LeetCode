283aae72b72e20584d5d2f37b1566b7aa283b859
WangLaban <42727870+WangLaban@users.noreply.github.com>
2018-09-19 14:23:46 +0800

    Create 70_ClimbingStairs.h

---
+++ 70_ClimbingStairs.h
@@ -0,0 +1,34 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Note: Given n will be a positive integer.
+Example 1:
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+*/
+// Solution: f(n) = f(n - 1) + f(n - 2)
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1 || n == 2)
+            return n;
+        int res = 0, res1 = 1, res2 = 2;
+        for(int i = 3; i <= n; i++)
+        {
+            res = res1 + res2;
+            res1 = res2;
+            res2 = res;
+        }
+        return res;
+    }
+};
