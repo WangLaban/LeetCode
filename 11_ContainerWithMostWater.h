185a935fe516a13a6484e2ed6435983e015f75aa
WangLaban <42727870+WangLaban@users.noreply.github.com>
2018-10-08 09:18:41 +0800

    Update 11_ContainerWithMostWater.h
    Add a solution

--- 11_ContainerWithMostWater.h
+++ 11_ContainerWithMostWater.h
@@ -6,11 +6,12 @@ Example:
 Input: [1,8,6,2,5,4,8,3,7]
 Output: 49
 */
+// Solution1: 
 /*
 Time complexity : O(n^2). Calculating area for all n * (n - 1) / 2 height pairs.
 Space complexity : O(1). Constant extra space is used. 
 */
-class Solution {
+/*class Solution {
 public:
     int maxArea(vector<int>& height) {
         int maxArea = 0;
@@ -21,6 +22,35 @@ public:
                 minValue = min(height[i], height[j]);
                 maxArea = max(maxArea, minValue * (j - i));
             }
+        
+        return maxArea;
+    }
+};*/
+
+// Solution2: 
+/*
+Time complexity : O(n). Single pass.
+Space complexity : O(1). Constant extra space is used. 
+*/
+// Initially we consider the area constituting the exterior most lines. Now, to maximize the area,
+// we need to consider the area between the lines of larger lengths. If we try to move the pointer
+// at the longer line inwards, we won't gain any increase in area, since it is limited by the shorter
+// line. But moving the shorter line's pointer could turn out to be beneficial, as per the same
+// argument, despite the reduction in the width. This is done since a relatively longer line obtained
+// by moving the shorter line's pointer might overcome the reduction in area caused by the width reduction.
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxArea = 0;
+        int left = 0, right = height.size() - 1;
+        while(left < right)
+        {
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left));
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
         
         return maxArea;
     }
