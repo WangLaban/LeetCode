92a73a2bd583def64af7335c697d90b81b76cf97
WangLaban <42727870+WangLaban@users.noreply.github.com>
2018-10-08 09:36:25 +0800

    Update 3_LongestSubstringWithoutRepeatingCharacters.h
    Add a solution

--- 3_LongestSubstringWithoutRepeatingCharacters.h
+++ 3_LongestSubstringWithoutRepeatingCharacters.h
@@ -14,6 +14,7 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3. 
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+// Solution1:
 /*
 Time complexity : O(2n) = O(n). In the worst case each character will be visited twice by i and j.
 Space complexity : O(min(m, n)). We need O(k) space for the sliding window, where k is the size of the Set. The size of the Set is upper bounded by the size of the string n and the size of the charset/alphabet m.  
@@ -41,6 +42,36 @@ public:
                 j++;
                 ans = max(ans, j - i);
             }
+        }
+        return ans;
+    }
+};
+
+// Solution2:
+/*
+Time complexity : O(n). Index j will iterate n times.
+Space complexity (HashMap) : O(min(m, n)).
+*/
+/*
+unordered_map, C++ 11 new feature, based on Hash table, it reduces the look up time to O(1) 
+*/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int len = s.length();
+        unordered_map<char, int> cmap;  //hash table
+        int ans = 0, i = 0, j = 0;
+
+        for(; j < len; j++)
+        {
+            //try to extend the range[i, j)
+            if(cmap.find(s[j]) != cmap.end())
+            {
+                i = max(cmap[s[j]], i);
+            }
+            ans = max(ans, j - i + 1);
+            cmap[s[j]] = j + 1;  //OK, exist key, will change its value
+            //cmap.insert(pair<char, int>(s[j], j + 1));  //error, exist key, won't change its value
         }
         return ans;
     }
