31becc1d74b1a6e1b23d9f25733d983750b7ede8
WangLaban <42727870+WangLaban@users.noreply.github.com>
2018-10-08 09:57:17 +0800

    Update 9_PalindromeNumber.h
    Add a solution

--- 9_PalindromeNumber.h
+++ 9_PalindromeNumber.h
@@ -15,7 +15,7 @@ Follow up:
 Coud you solve it without converting the integer to a string?
 */
 //Solution1: mySolution
-class Solution {
+/*class Solution {
 public:
     bool isPalindrome(int x) {
         if(x < 0)
@@ -35,5 +35,36 @@ public:
             return false;
         }
         return true;
+    }
+};*/
+
+// Solution2:
+/*
+Time complexity : O(lg(n)). We divided the input by 10 for every iteration, so the time complexity is O(lg(n)).
+Space complexity : O(1).
+*/
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Special cases:
+        // When x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if(x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+        
+        int revertedNumber = 0;
+        while(x > revertedNumber)
+        {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply
+        // get rid of it.
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 };
