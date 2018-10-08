7b485218e5c4112cbbac182fe7f12a93df48b086
WangLaban <42727870+WangLaban@users.noreply.github.com>
2018-10-08 09:26:45 +0800

    Update 22_GenerateParentheses.h
    Add a solution

--- 22_GenerateParentheses.h
+++ 22_GenerateParentheses.h
@@ -9,11 +9,12 @@ For example, given n = 3, a solution set is:
   "()()()"
 ]
 */
+// Solution1:
 /*
 Time Complexity : O(4^n / √n). Each valid sequence has at most n steps during the backtracking procedure.
 Space Complexity : O(4^n / √n), as described above, and using O(n) space to store the sequence. 
 */
-void backtrack(vector<string> &ans, string cur, int open, int close, int max)
+/*void backtrack(vector<string> &ans, string cur, int open, int close, int max)
 {
     if(cur.length() == max * 2)
     {
@@ -36,6 +37,33 @@ public:
         if(n == 0)
             return ret;
         backtrack(ret, "", 0, 0, n);
+        return ret;
+    }
+};*/
+
+// Solution2:
+/*
+Time Complexity : O(4^n / √n).
+Space Complexity : O(4^n / √n).
+*/
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ret;
+        set<string> mySet;
+        if(n == 0)
+            ret.push_back("");
+
+        for(int i = 0; i < n; i++)
+        {
+            for(string left: generateParenthesis(i))
+            {
+                for(string right: generateParenthesis(n - 1 - i))
+                {
+                    ret.push_back("(" + left + ")" + right);
+                }
+            }
+        }
         return ret;
     }
 };
