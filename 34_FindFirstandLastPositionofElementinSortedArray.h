c7295af2f55819d8ef078c3ba5bf1cde1bc32518
WangLaban <42727870+WangLaban@users.noreply.github.com>
2018-10-08 09:34:43 +0800

    Update 34_FindFirstandLastPositionofElementinSortedArray.h

--- 34_FindFirstandLastPositionofElementinSortedArray.h
+++ 34_FindFirstandLastPositionofElementinSortedArray.h
@@ -9,13 +9,14 @@ Example 2:
 Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
 */
+// Solution1:
 /*
 Time complexity : O(n)
 This brute-force approach examines each of the n elements of nums exactly twice, so the overall runtime is linear.
 Space complexity : O(1)
 The linear scan method allocates a fixed-size array and a few integers, so it has a constant-size memory footprint.
 */
-class Solution {
+/*class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ivec{-1, -1};
@@ -43,6 +44,43 @@ public:
             }
         }
 
+        return ivec;
+    }
+};*/
+
+// Solution2:
+/*
+Time complexity : O(log(n))
+Because binary search cuts the search space roughly in half on each iteration, there can be at most [log(n)] iterations. Binary search is invoked twice, so the overall complexity is logarithmic.
+Space complexity : O(1)
+All work is done in place, so the overall memory usage is constant.
+*/
+// returns leftmost (or rightmost) index at which `target` should be
+// inserted in sorted array `nums` via binary search.
+int extremeInsertionIndex(vector<int>& nums, int target, bool left)
+{
+    int low = 0, high = nums.size();  // The first element's index and the past-the-end element's index
+    while(low < high)
+    {
+        int mid = (low + high) / 2;
+        if(nums[mid] > target || (left && nums[mid] == target))
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    return low;
+}
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ivec{-1, -1};
+        int left = extremeInsertionIndex(nums, target, true);
+        if(left == nums.size() || nums[left] != target)
+            return ivec;
+        ivec.clear();
+        ivec.push_back(left);
+        ivec.push_back(extremeInsertionIndex(nums, target, false) - 1);
+        
         return ivec;
     }
 };
